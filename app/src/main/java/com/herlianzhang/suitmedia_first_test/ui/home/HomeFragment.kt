ce1f91bfef907ff22aa6d08371cd1efd9240c09c
herlianzhang <herlianzhang370@gmail.com>
2021-07-10 18:29:55 +0700

    Question 1

--- app/src/main/java/com/herlianzhang/suitmedia_first_test/ui/home/HomeFragment.kt
+++ app/src/main/java/com/herlianzhang/suitmedia_first_test/ui/home/HomeFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.herlianzhang.suitmedia_first_test.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +15,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+
+    private var dialog: AlertDialog? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,9 +32,30 @@ class HomeFragment : Fragment() {
     private fun initListener() {
         binding.buttonMain.setOnClickListener {
             val name = binding.etMain.text.toString().trim()
-            val action = HomeFragmentDirections.actionHomeFragmentToEventOrGuestFragment(name)
-            findNavController().navigate(action)
+            val message = if (isPalindrome(name)) "isPalindrome" else "not palindrome"
+
+            dialog?.dismiss()
+            dialog = MaterialAlertDialogBuilder(requireContext())
+                .setTitle(name)
+                .setMessage(message)
+                .setPositiveButton("Okay") { _, _ ->
+                    val action =
+                        HomeFragmentDirections.actionHomeFragmentToEventOrGuestFragment(name)
+                    findNavController().navigate(action)
+                }.show()
         }
+    }
+
+    private fun isPalindrome(value: String): Boolean {
+        var isPalindrome = true
+        val text = value.replace(" ", "").lowercase()
+        for (i in 0 until (text.count() / 2)) {
+            if (text[i] != text[text.count() - 1 - i]) {
+                isPalindrome = false
+                break
+            }
+        }
+        return isPalindrome
     }
 
 }
